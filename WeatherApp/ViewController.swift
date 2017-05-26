ac1b96f2898b030dbce07f5214d8d89c4d6fe222
Tanja Keune <tanjakeune@gmail.com>
2017-05-26 13:59:02 -0400

    basic functionality

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var locationText: UITextField!
+    @IBOutlet var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +24,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitButton(_ sender: AnyObject) {
+        
+        if let url = URL(string: "http://www.weather-forecast.com/locations/" + locationText.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
+        let request = NSMutableURLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            var message = ""
+            if error != nil {
+                print(error)
+            } else {
+                if let unwrappedData = data {
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                    if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                        if contentArray.count > 1 {
+                            stringSeparator = "</span>"
+                            let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                            if newContentArray.count > 1 {
+                                
+                                message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                            }
+                        }
+                    }
+                }
+                
+                if message == "" {
+                    message = "The weather there couldn't be found. Please try again."
+                }
+                DispatchQueue.main.sync(execute: {
+                    self.resultLabel.text = message
+                })
+            }
+            
+            
+        }
+        task.resume()
 
+        } else {
+            resultLabel.text = "The weather there couldn't be found. Please try again."
+
+        }
+    }
+    
 }
 
